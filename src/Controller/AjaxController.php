6382ff1289c613b3f1f6ce8fd494d7bdb44d511f
Miarimalala <tokinambinina@gmail.com>
2022-01-16 14:06:31 +0100

    Formulaire Ajout dirigeant terminé

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -1,19 +1,52 @@
 <?php
     namespace App\Controller;
 
+use App\Entity\Dirigeant;
+use App\Repository\DirigeantRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class AjaxController{
         
         /**
-         * @Route("/ajoutDirigeant", name = "ajout_dirigeant")
+         * @Route("/ajoutDirigeant", name = "ajout_dirigeant", methods={"POST"})
          */
-        public function ajoutDirigeant(Request $request, LoggerInterface $logger){
+        public function ajoutDirigeant(DirigeantRepository $dirigeantRepository, SerializerInterface $serializer, LoggerInterface $logger, Request $request, EntityManagerInterface $em){
             
-            //$jsonRecu = $request->getContent();
-            $logger->error("tonga77 :  ");
+            //*
+            $dirigeants = $dirigeantRepository->findAll();
+            $json = $serializer->serialize($dirigeants,'json');
+            
+            $logger->error("==========================77==========================");
+            $logger->error($json);
+            //$logger->debug("DBG",$json);
+            $logger->error("====================================================");
+            //*/
+
+
+            $jsonRecu = $request->getContent();
+
+            $logger->error("=======================78=============================");
+            $logger->error($jsonRecu);
+            $logger->error("====================================================");
+            //$jsonRecu2=" {'nomPrenom':'Rakoto','adresseMail':'rakotoyahoo.fr','sexe':'homme'} ";
+            $dirigeant = $serializer->deserialize($jsonRecu, Dirigeant::class,'json');
+            //$logger->info($dirigeant->getNomPrenom());
+
+            $logger->error("=========================79===========================");
+            $logger->error($dirigeant->getNomPrenom());
+            $logger->error("====================================================");
+
+            $em->persist($dirigeant);
+            $em->flush();
+
+            //$logger->error("tonga78 :  ");
+            //die();
+            
+
             die();
         }
 
